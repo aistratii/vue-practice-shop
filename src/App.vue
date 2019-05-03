4d9549e29da5dd36cfba536277808badb8735a41
Alexandru ISTRATII <aistratii@pentalog.com>
2019-05-03 10:08:04 +0300

    Create and use child components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-for="product in products" :key="product.id">
+            <product></product>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import products from "@/products.json";
+import Product from "@/components/Product.vue";
+
+  export default {
+    name: "app",
+
+    components: {
+      Product
+    },
+
+    data() {
+      return {
+          products
+      }
+    }
+  }
 </script>
 
 <style>
-
+  body {
+    background-color: #FBF8F3;
+  }
 </style>
